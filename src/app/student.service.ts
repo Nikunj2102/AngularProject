9f8c30e92ba4029c699269acd954344230153ebe
Nikunj2102 <nikunjdhingra21@gmail.com>
2019-06-11 14:50:03 +0530

    InMemoryDbWorking

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { student } from './student';
 import { Observable , of} from 'rxjs';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { STUDENT_DATA } from './dummyData';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentService {
 
+export class StudentService {
+  private url = '/api/STUDENT_DATA'
   getStudents() : Observable<student[]>
   {
-    return of(STUDENT_DATA);
+    return this.http.get<student[]>(this.url); 
+    //of(STUDENT_DATA);
   }
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
